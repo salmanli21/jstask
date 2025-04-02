a23ca88fcd5b28bdee3f5d8dc453d98b678dfd4b
salmanli21 <sahverdiss-bp218@code.edu.az>
2025-04-02 19:38:38 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// 1-100 aralığında sonu 9 ilə bitən sayların sayını tapın
+// let cem = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 10 === 9) {
+//      cem++
+//     }
+// }
+
+// console.log(cem)
+// 3 tam eded verilmisdir onlarin duzbucaqli ucbucaq yaradan programini teyin edin
+// let a =+prompt("BIRINCI TEREF")
+// let b =+prompt("IKINCI TEREF")
+// let c =+prompt("UCUNCU TEREF")
+// if ((a*a)+(b*b)===(c*c)) { 
+//     console.log("duzbucaqli ucbucaqdir");
+// }
+//     else{
+//         console.log("duzbucaqli ucbucaq deyil");
+        
+//     }
+
+// for (let i = 1; i <= 10; i++) {
+
+//     for (let j = 1; j <= 10; j++) {
+//         let vurma= `${i} * ${j}=${i*j} `
+//     console.log(vurma);
+// }
+// }
